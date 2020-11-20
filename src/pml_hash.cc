db6f4c724d446972037f8791a650e4bd72fd228a
Your Name <you@example.com>
2020-11-20 09:12:34 +0000

    style: 优化布局，增加数行编译测试代码
    将源文件移动到src文件夹下，修改了CMake配置，增加了gitignore，取消main函
    数注释，填写了几个方法的测试用空返回值

--- src/pml_hash.cc
+++ src/pml_hash.cc
@@ -6,15 +6,16 @@
  * if the data file exist, open it and recover the hash
  * if the data file does not exist, create it and initial the hash
  */
-PMLHash::PMLHash(const char* file_path) {
-
+PMLHash::PMLHash(const char *file_path)
+{
 }
 /**
  * PMLHash::~PMLHash 
  * 
  * unmap and close the data file
  */
-PMLHash::~PMLHash() {
+PMLHash::~PMLHash()
+{
     pmem_unmap(start_addr, FILE_SIZE);
 }
 /**
@@ -23,13 +24,13 @@ PMLHash::~PMLHash() {
  * split the hash table indexed by the meta->next
  * update the metadata
  */
-void PMLHash::split() {
+void PMLHash::split()
+{
     // fill the split table
 
     // fill the new table
 
     // update the next of metadata
-
 }
 /**
  * PMLHash 
@@ -41,8 +42,9 @@ void PMLHash::split() {
  * need to hash the key with proper hash function first
  * then calculate the index by N module
  */
-uint64_t PMLHash::hashFunc(const uint64_t &key, const size_t &hash_size) {
-
+uint64_t PMLHash::hashFunc(const uint64_t &key, const size_t &hash_size)
+{
+    return key; //for test
 }
 
 /**
@@ -52,8 +54,10 @@ uint64_t PMLHash::hashFunc(const uint64_t &key, const size_t &hash_size) {
  *                             to the start of the whole file
  * @return {pm_table*}       : the virtual address of new overflow hash table
  */
-pm_table* PMLHash::newOverflowTable(uint64_t &offset) {
-
+pm_table *PMLHash::newOverflowTable(uint64_t &offset)
+{
+    pm_table test; //for test
+    return &test;  //for test
 }
 
 /**
@@ -69,8 +73,9 @@ pm_table* PMLHash::newOverflowTable(uint64_t &offset) {
  * 
  * if the hash table is full then split is triggered
  */
-int PMLHash::insert(const uint64_t &key, const uint64_t &value) {
-
+int PMLHash::insert(const uint64_t &key, const uint64_t &value)
+{
+    return 0; //for test
 }
 
 /**
@@ -82,8 +87,9 @@ int PMLHash::insert(const uint64_t &key, const uint64_t &value) {
  * 
  * search the target entry and return the value
  */
-int PMLHash::search(const uint64_t &key, uint64_t &value) {
-
+int PMLHash::search(const uint64_t &key, uint64_t &value)
+{
+    return 0; //for test
 }
 
 /**
@@ -95,8 +101,9 @@ int PMLHash::search(const uint64_t &key, uint64_t &value) {
  * remove the target entry, move entries after forward
  * if the overflow table is empty, remove it from hash
  */
-int PMLHash::remove(const uint64_t &key) {
-
+int PMLHash::remove(const uint64_t &key)
+{
+    return 0; //for test
 }
 
 /**
@@ -108,6 +115,7 @@ int PMLHash::remove(const uint64_t &key) {
  * 
  * update an existing entry
  */
-int PMLHash::update(const uint64_t &key, const uint64_t &value) {
-
+int PMLHash::update(const uint64_t &key, const uint64_t &value)
+{
+    return 0; //for test
 }
