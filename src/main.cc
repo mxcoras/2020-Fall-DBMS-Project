9fc1cd7e799cb92c6f8eda145b4cc4d162e46fca
690865124Pv <807759083@qq.com>
2020-12-19 13:08:15 +0000

    feat: sixth

--- src/main.cc
+++ src/main.cc
@@ -1,55 +1,48 @@
 #include "pml_hash.h"
+#include <iomanip>
 #include <fstream>
 #include <iostream>
 #include <dirent.h>
-#include <regex>
 #include <vector>
-#include <string.h>
-#include <cstdio>
-#include <time.h>
-#include <iomanip>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+
 using namespace std;
 
+//存放文件夹中的文件的名称
 vector <string> Filelist;
-int testNum;
-string dir_name = "../benchmark/";
+//文件夹的名称
+const string dir_name = "../benchmark/";
 
-uint64_t change_to_uint64(string s){
-    uint64_t ret=0;
-    for (int i=0; i<8; i++){
-        ret <<= 8;
-        ret +=s[i];
-    }
-    return ret;
-}
-void getFileList(){
+void OpenDir(){
     DIR *dir;
-    struct dirent *dirp;
+    struct dirent *mydirent;
     //判断文件夹打开是否成功
     if ((dir=opendir(dir_name.c_str()))==NULL){
         cout << "fail to open " << dir_name << endl; 
     }
     //取出文件夹中的文件
-    while ((dirp = readdir(dir)) != NULL){
-        Filelist.push_back(dirp->d_name);
+    while ((mydirent = readdir(dir)) != NULL){
+        // d_type为8，表示读入的文件类型为txt
+        if ((int)mydirent->d_type==8)
+            Filelist.push_back(mydirent->d_name);
     }
     //关闭文件夹
     closedir(dir);
     //排序，使得read和write比例相同的read和load文件放在一起，
     //方便后续执行
     sort(Filelist.begin(),Filelist.end());
-    //load和run各占一半
-    testNum=Filelist.size()/2;
-    for (int i=0; i<testNum; i++){
-        cout << "[FIND FILE] " << Filelist[i*2] << " and " << Filelist[i*2+1] << endl;
+    for (uint64_t i=0; i<Filelist.size(); i++){
+        cout << "filename: " << Filelist[i] << endl;
     }
+    cout << endl;
 }
-void loadFile(string filename, PMLHash& hash){
+void Load(string filename, PMLHash& hash){
     //打开文件
     fstream file;
     file.open(dir_name + filename, ios::in);
-    
-    cout << "start to init with " << filename << endl;
+    cout << "start to init" << endl;
     string cmd,data;
     time_t start = clock();
     //记录数据量
@@ -58,15 +51,14 @@ void loadFile(string filename, PMLHash& hash){
     long long failNum = 0;
     //开始读取文件
     while (!file.eof()){
-        //清空数据
-        cmd = data = "";
+        cmd="";
         //读入操作符，若为空表示终止
         file >> cmd;
-        if (cmd == "") break;
+        if (cmd.empty()) break;
         file >> data;
-        //由于取8个字节为键值，所以取数字字符串的前8个字符，每一个字符占一个字节，换算成uint64_t
-        uint64_t key = change_to_uint64(data.substr(0,8));
-        uint64_t value = atoi(data.substr(8).c_str());
+        //key和value的值相同
+        uint64_t key = atoi(data.c_str());
+        uint64_t value =value ;
         //由于为load，只有插入操作
         //进行插入操作
         if (hash.insert(key,value)==-1) failNum++;
@@ -74,16 +66,17 @@ void loadFile(string filename, PMLHash& hash){
     }
     time_t end = clock();
     file.close();
-    cout << "init finished, use time: " <<  fixed << setprecision(5) << ((double)(end-start))/CLOCKS_PER_SEC << endl;
+    double cost = ((double)(end-start))/CLOCKS_PER_SEC;
+    cout << "Finished, use time: " <<  fixed << setprecision(5) << cost << "s" << endl;
     cout << "total number: " << dataNum << endl;
     cout << "fail number: " << failNum << endl;
+    cout << endl;
 }
-void runFile(string filename, PMLHash& hash){
+void Run(string filename, PMLHash& hash){
     //打开文件
     fstream file;
     file.open(dir_name + filename, ios::in);
-    
-    cout << "start to init with " << filename << endl;
+    cout << "start to run " << endl;
     string cmd,data;
     time_t start = clock();
     //记录各个操作数据量
@@ -92,15 +85,14 @@ void runFile(string filename, PMLHash& hash){
     long long failNum = 0;
     //开始读取文件
     while (!file.eof()){
-        //清空数据
-        cmd = data = "";
+        cmd="";
         //读入操作符，若为空表示终止
         file >> cmd;
-        if (cmd == "") break;
+        if (cmd.empty()) break;
         file >> data;
-        //由于取8个字节为键值，所以取数字字符串的前8个字符，每一个字符占一个字节，换算成uint64_t
-        uint64_t key = change_to_uint64(data.substr(0,8));
-        uint64_t value = atoi(data.substr(8).c_str());
+        //key和value的值相同
+        uint64_t key = atoi(data.c_str());
+        uint64_t value = value;
         if (cmd == "INSERT"){
             if (hash.insert(key, value)== -1) failNum++;
             insertNum++;
@@ -118,29 +110,36 @@ void runFile(string filename, PMLHash& hash){
             removeNum++;
         }
         else {
-            cout << "[error] invalid operation " << endl;
+            cout << "invalid operation " << endl;
         }
         totalNum++;
     }
     time_t end = clock();
     file.close();
-    double spendtime = ((double)(end-start))/CLOCKS_PER_SEC;
-    cout << "run finished, use time: " <<  fixed << setprecision(5) << spendtime << endl;
+    double cost = ((double)(end-start))/CLOCKS_PER_SEC;
+    cout << "Finished, use time: " <<  fixed << setprecision(5) << cost << "s" << endl;
     cout << "total number: " << totalNum << endl;
     cout << "insert/total: " <<  (double)insertNum/(double)totalNum << endl;
     cout << "read/total: " <<  (double)searchNum/(double)totalNum << endl;
     cout << "update/total: " <<  (double)updateNum/(double)totalNum << endl;
     cout << "remove/total: " <<  (double)removeNum/(double)totalNum << endl;
     cout << "fail/total: " << (double)failNum/(double)totalNum << endl;
-    cout << "OPS: " << (long long)(totalNum/spendtime) << endl;
+    cout << "OPS: " << (long long)(totalNum/cost) << endl;
+    cout << endl;
 }
 int main()
 {
-    getFileList();
-    for (int i=0; i<testNum; i++){
+    //打开文件夹
+    OpenDir();
+    for (uint64_t i=0; i<Filelist.size()/2; i++){
+        cout << "Test " << i << endl;
+        cout << "operation with " << Filelist[2*i] << " and " << Filelist[2*i+1] << endl;
+        //依次对每对load和run文件进行操作
         PMLHash hash("/mnt/pmemdir/file");
-        loadFile(Filelist[2*i], hash);
-        runFile(Filelist[2*i+1], hash);
+        Load(Filelist[2*i], hash);
+        Run(Filelist[2*i+1], hash);
+        //清空上个文件对的内容
+        system("rm /mnt/pmemdir/file");
     }
     return 0;
 }
