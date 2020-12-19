d1f4c39c42efa6741a9ac7ce3d6c4434950c3bbc
690865124Pv <807759083@qq.com>
2020-12-19 11:14:11 +0000

    feat: third

--- src/main.cc
+++ src/main.cc
@@ -7,10 +7,21 @@
 #include <string.h>
 #include <cstdio>
 #include <time.h>
+#include <iomanip>
+using namespace std;
 
 vector <string> Filelist;
 int testNum;
 string dir_name = "../benchmark/";
+
+uint64_t change_to_uint64(string s){
+    uint64_t ret=0;
+    for (int i=0; i<8; i++){
+        ret <<= 8;
+        ret +=s[i];
+    }
+    return ret;
+}
 void getFileList(){
     DIR *dir;
     struct dirent *dirp;
@@ -41,7 +52,10 @@ void loadFile(string filename, PMLHash& hash){
     cout << "start to init with " << filename << endl;
     string cmd,data;
     time_t start = clock();
+    //记录数据量
     long long dataNum = 0;
+    //记录插入失败的数据量
+    long long failNum = 0;
     //开始读取文件
     while (!file.eof()){
         //清空数据
@@ -50,8 +64,72 @@ void loadFile(string filename, PMLHash& hash){
         file >> cmd;
         if (cmd == "") break;
         file >> data;
-        
+        //由于取8个字节为键值，所以取数字字符串的前8个字符，每一个字符占一个字节，换算成uint64_t
+        uint64_t key = change_to_uint64(data.substr(0,8));
+        uint64_t value = atoi(data.substr(8).c_str());
+        //由于为load，只有插入操作
+        //进行插入操作
+        if (hash.insert(key,value)==-1) failNum++;
+        dataNum++;
     }
+    time_t end = clock();
+    file.close();
+    cout << "init finished, use time: " <<  fixed << setprecision(5) << ((double)(end-start))/CLOCKS_PER_SEC << endl;
+    cout << "total number: " << dataNum << endl;
+    cout << "fail number: " << failNum << endl;
+}
+void runFile(string filename, PMLHash& hash){
+    //打开文件
+    fstream file;
+    file.open(dir_name + filename, ios::in);
+    
+    cout << "start to init with " << filename << endl;
+    string cmd,data;
+    time_t start = clock();
+    //记录各个操作数据量
+    long long insertNum = 0, searchNum = 0, updateNum = 0, removeNum = 0, totalNum = 0;
+    //记录操作失败的数据量
+    long long failNum = 0;
+    //开始读取文件
+    while (!file.eof()){
+        //清空数据
+        cmd = data = "";
+        //读入操作符，若为空表示终止
+        file >> cmd;
+        if (cmd == "") break;
+        file >> data;
+        //由于取8个字节为键值，所以取数字字符串的前8个字符，每一个字符占一个字节，换算成uint64_t
+        uint64_t key = change_to_uint64(data.substr(0,8));
+        uint64_t value = atoi(data.substr(8).c_str());
+        if (cmd == "INSERT"){
+            if (hash.insert(key, value)== -1) failNum++;
+            insertNum++;
+        }
+        else if (cmd == "READ"){
+            if (hash.search(key, value)== -1) failNum++;
+            searchNum++;
+        }
+        else if (cmd == "UPDATE"){
+            if (hash.update(key, value)== -1) failNum++;
+            updateNum++;
+        }
+        else if (cmd == "REMOVE"){
+            if (hash.remove(key)== -1) failNum++;
+            removeNum++;
+        }
+        else {
+            cout << "[error] invalid operation " << endl;
+        }
+        totalNum++;
+    }
+    time_t end = clock();
+    file.close();
+    cout << "run finished, use time: " <<  fixed << setprecision(5) << ((double)(end-start))/CLOCKS_PER_SEC << endl;
+    cout << "insert/total: " <<  (double)insertNum/(double)totalNum << endl;
+    cout << "read/total: " <<  (double)searchNum/(double)totalNum << endl;
+    cout << "update/total: " <<  (double)updateNum/(double)totalNum << endl;
+    cout << "remove/total: " <<  (double)removeNum/(double)totalNum << endl;
+    cout << "fail/total: " << (double)failNum/(double)totalNum << endl;
 }
 int main()
 {
